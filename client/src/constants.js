b567b1c72b3e89001f277cde4fe76273992c064a
Vittorio Rossitto <v.s.r@abv.bg>
2022-07-30 17:12:55 +0300

    Define site name

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,8 @@
+const NAME = "Papyrus Books";
+const SLOGAN = "";
+
 const DB_HOST_URL = "http://localhost:5000/books";
 const API_KEY = "AIzaSyCXSS5mQccmJBZYj-DOkxO3E6fIALx56us";
 const BOOKS_BASE_URL = "https://www.googleapis.com/books/v1/volumes";
 
-export { DB_HOST_URL, API_KEY, BOOKS_BASE_URL };
+export { DB_HOST_URL, API_KEY, BOOKS_BASE_URL, NAME, SLOGAN };
