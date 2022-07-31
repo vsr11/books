681143e16e699daf17255f6ba96dfd30c91b6bc2
Vittorio Rossitto <v.s.r@abv.bg>
2022-07-31 17:37:33 +0300

    Add login and register routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 
-// import './App.css';
+import "./App.css";
 
 import EditBook from "./components/EditBook";
 import DeleteBook from "./components/DeleteBook";
 import Main from "./components/Main";
 import AddBook from "./components/AddBook";
 import BookInfo from "./components/BookInfo";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/add" element={<AddBook />} />
         <Route path="/info/:id" element={<BookInfo />} />
         <Route path="/edit/:id" element={<EditBook />} />
