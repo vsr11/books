428dee72ee7723ee290540d46e57bc5149258112
Vittorio Rossitto <v.s.r@abv.bg>
2022-07-31 17:40:40 +0300

    Add user data location constant

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,8 +1,9 @@
-const NAME = "Papyrus Books";
+const NAME = "";
 const SLOGAN = "";
 
 const DB_HOST_URL = "http://localhost:5000/books";
 const API_KEY = "AIzaSyCXSS5mQccmJBZYj-DOkxO3E6fIALx56us";
 const BOOKS_BASE_URL = "https://www.googleapis.com/books/v1/volumes";
+const USERS_HOST_URL = "http://localhost:5000";
 
-export { DB_HOST_URL, API_KEY, BOOKS_BASE_URL, NAME, SLOGAN };
+export { DB_HOST_URL, API_KEY, BOOKS_BASE_URL, NAME, SLOGAN, USERS_HOST_URL };
