4dcfa2e31b22181789a9d30a35f8ad18e61161f0
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-03 15:01:24 +0300

    Add Stars component

---
+++ client/src/components/Stars.js
@@ -0,0 +1,32 @@
+import { ratingCalc } from "../utils/ratingCalc";
+import "../styles/Stars.css";
+
+const Stars = (props) => {
+  const [arr, s] = [props.arr, Number(props.starpx)];
+  return (
+    <div>
+      <span
+        className="rating-box"
+        style={{
+          width: (s + 1) * 5 + 1,
+          height: s + 1,
+          backgroundSize: s + 1,
+        }}
+      >
+        <span
+          className="rating"
+          style={{
+            width:
+              ((arr ? ratingCalc.averageRating(arr).toFixed(2) : 0) *
+                ((s + 1) * 5 + 1)) /
+              5,
+            height: s + 1,
+            backgroundSize: s + 1,
+          }}
+        ></span>
+      </span>
+    </div>
+  );
+};
+
+export default Stars;
