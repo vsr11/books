ece861a520114e9635a8d7b24a2973e7be5360de
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-03 15:07:02 +0300

    Fix rating calculation function

--- client/src/utils/ratingCalc.js
+++ client/src/utils/ratingCalc.js
@@ -4,12 +4,12 @@ const ratingCalc = {
   },
 
   averageRating: (arr) => {
-    if (ratingCalc.ratingCount() === 0) {
+    if (ratingCalc.ratingCount(arr) === 0) {
       return 0;
     } else {
       return (
         (arr[0] * 5 + arr[1] * 4 + arr[2] * 3 + arr[3] * 2 + arr[4]) /
-        (arr[0] + arr[1] + arr[2] + arr[3] + arr[4])
+        ratingCalc.ratingCount(arr)
       );
     }
   },
