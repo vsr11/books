001b871df4b53a4abd08f28f9b4941aa97973b3e
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-11 15:33:09 +0300

    Add sorting, title, and rating to categories

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,36 +1,58 @@
 import { useEffect, useState } from "react";
-import { Link, NavLink, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import BookItem from "./hoc/BookItem";
 import internal from "../services/internal";
-import { categories } from "../utils/categories";
 import "../styles/Main.css";
+import { useSearchParams } from "react-router-dom";
+import { ratingCalc } from "../utils/ratingCalc";
 
 const Categories = () => {
   const params = useParams();
+  let [searchParams] = useSearchParams();
+  let sort = searchParams.get("sort");
   const [state, setState] = useState([]);
+  let title1 = "";
+  let info1 = (x) => null;
+
   useEffect(() => {
     internal.getByCategories("categories", params.category).then((books) => {
       setState(books);
     });
-  }, [params.category]);
+  }, [params]);
+
+  if (sort === "sortCount") {
+    state.sort((item1, item2) => {
+      return (
+        ratingCalc.ratingCount(item2.rating) -
+        ratingCalc.ratingCount(item1.rating)
+      );
+    });
+    title1 = "Most read books";
+    info1 = (x) => ratingCalc.ratingCount(x.rating) + "   votes";
+  }
+
+  if (sort === "sortAv") {
+    state.sort((item1, item2) => {
+      return (
+        ratingCalc.averageRating(item2.rating) -
+        ratingCalc.averageRating(item1.rating)
+      );
+    });
+    title1 = "Highest rated books";
+    info1 = (x) => "rating : " + ratingCalc.averageRating(x.rating);
+  }
 
   return (
     <div id="site">
-      <div className="categories">
-        <h2>CATEGORIES</h2>
-        <div className="item-categories">
-          <ul>
-            {categories.map((x) => (
-              <li>
-                <NavLink to={`/categories/${x}`}>{x}</NavLink>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
       <div id="main">
+        <div className="title">
+          {title1} {params.category}
+        </div>
         {state?.map((x) => (
-          <BookItem key={x.id} {...x} />
+          <div key={x.id} style={{ width: "20%", margin: "1rem" }}>
+            <div className="info">{info1(x)}</div>
+            <BookItem key={x.id} {...x} />
+          </div>
         ))}
       </div>
       <Link
