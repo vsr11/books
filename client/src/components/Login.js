9ef5ba2ec6b86980c5763310114fcf007e18e4b8
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-11 15:36:26 +0300

    Change from username to email and check if user exists

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,32 +1,50 @@
-import { USERS_HOST_URL } from "../constants";
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import internal_api from "../services/internal";
+import AuthContext from "../contexts/Auth";
+import "../styles/Login.css";
 
 const Login = () => {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [err, setErr] = useState("");
+
   const onSubmitLoginHandle = (e) => {
     e.preventDefault();
 
-    let u = document.forms[0].username.value;
+    let u = document.forms[0].email.value;
     let p = document.forms[0].password.value;
+    // auth.isLoggedIn = true;
 
-    if (u !== "" && p !== "") {
-      return fetch(USERS_HOST_URL + "/signin", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email: u, password: p }),
-      })
-        .then((res) => res.json())
-        .then((res) => console.log(res));
-    }
+    internal_api.userExists(u).then((res) => {
+      if (res.length !== 0) {
+        auth.user = res[0];
+        auth.isLoggedIn = true;
+        navigate("/");
+      } else {
+        setErr("Invalid email, name, or password");
+      }
+    });
   };
 
   return (
-    <form method="POST" onSubmit={onSubmitLoginHandle}>
-      <label htmlFor="username">Username:</label>
-      <input type="text" id="username" name="username" />
-      <label htmlFor="password">Password:</label>
-      <input type="password" id="password" name="password" />
-      <input type="submit" value="Login!" />
-      <input type="reset" value="Reset!" />
-    </form>
+    <div className="login">
+      <form onSubmit={onSubmitLoginHandle}>
+        <div>
+          <label htmlFor="email">E-mail:</label>
+          <input type="text" id="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <div>
+          <input type="submit" value="Login" />
+          <input type="reset" value="Reset" />
+        </div>
+      </form>
+      <div className="err">{err}</div>
+    </div>
   );
 };
 
