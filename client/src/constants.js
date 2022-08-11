af323fa4d08b177f2634ea236dfcbbf1add4dcbc
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-11 15:27:45 +0300

    Update constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -4,6 +4,15 @@ const SLOGAN = "";
 const DB_HOST_URL = "http://localhost:5000/books";
 const API_KEY = "AIzaSyCXSS5mQccmJBZYj-DOkxO3E6fIALx56us";
 const BOOKS_BASE_URL = "https://www.googleapis.com/books/v1/volumes";
-const USERS_HOST_URL = "http://localhost:5000";
+const USERS_HOST_URL = "http://localhost:5000/users";
+const HOST_URL_BASE = "http://localhost:5000";
 
-export { DB_HOST_URL, API_KEY, BOOKS_BASE_URL, NAME, SLOGAN, USERS_HOST_URL };
+export {
+  DB_HOST_URL,
+  API_KEY,
+  BOOKS_BASE_URL,
+  NAME,
+  SLOGAN,
+  USERS_HOST_URL,
+  HOST_URL_BASE,
+};
