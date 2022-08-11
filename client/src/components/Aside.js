5abb7113a9ea38d8ce62aa80532d72665b216a37
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-11 15:29:13 +0300

    Add sorting links to Aside component

--- client/src/components/Aside.js
+++ client/src/components/Aside.js
@@ -1,22 +1,55 @@
 import { categories } from "../utils/categories";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link, useSearchParams } from "react-router-dom";
+import "../styles/Aside.css";
 
 const Aside = () => {
+  let activeClassName = "active";
+
+  const [q] = useSearchParams();
+  const sort = q.get("sort");
+
+  let isActive1 = sort === "sortCount";
+  let isActive2 = sort === "sortAv";
+
   return (
-    <div className="categories">
-      <h2>CATEGORIES</h2>
-      <div className="item-categories">
+    <div className="aside">
+      <div className="categories">
+        <h2>CATEGORIES</h2>
+        <div className="item-categories">
+          <ul>
+            {categories.map((x, idx) => (
+              <li key={idx}>
+                <NavLink to={`/categories/${x}`}>{x}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="sort">
+        <h1>Rating</h1>
         <ul>
-          {categories.map((x, idx) => (
-            <li key={idx}>
-              <NavLink to={`/categories/${x}`}>{x}</NavLink>
-            </li>
-          ))}
+          <li>
+            <Link
+              to="?sort=sortCount"
+              className={isActive1 ? activeClassName : ""}
+            >
+              Most read books
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              to="?sort=sortAv"
+              className={isActive2 ? activeClassName : ""}
+            >
+              Highest rated books
+            </Link>
+          </li>
         </ul>
       </div>
-      <NavLink to="/sort1">Most read books</NavLink>
-      <NavLink to="/sort2">Highest rated books</NavLink>
     </div>
   );
 };
+
 export default Aside;
