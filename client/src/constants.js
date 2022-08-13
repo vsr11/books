bdb50cde796ab23e37d08bd8eb90beeb0dd73460
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-13 16:27:45 +0300

    localhost -> 127

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,11 +1,11 @@
 const NAME = "BOOKS";
 const SLOGAN = "";
 
-const DB_HOST_URL = "http://localhost:5000/books";
+const DB_HOST_URL = "http://127.0.0.1:5000/books";
 const API_KEY = "AIzaSyCXSS5mQccmJBZYj-DOkxO3E6fIALx56us";
 const BOOKS_BASE_URL = "https://www.googleapis.com/books/v1/volumes";
-const USERS_HOST_URL = "http://localhost:5000/users";
-const HOST_URL_BASE = "http://localhost:5000";
+const USERS_HOST_URL = "http://127.0.0.1:5000/users";
+const HOST_URL_BASE = "http://127.0.0.1:5000";
 
 export {
   DB_HOST_URL,
