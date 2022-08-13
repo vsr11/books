6da55bc94c4a32e5398c444cd0b1e5f8eff16211
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-13 16:29:47 +0300

    Add external links to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,12 @@
 import "../styles/Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <a href="https://www.goodreads.com/">Goodreads</a>
+      <a href="https://books.google.com/">Google Books</a>
+    </div>
+  );
 };
 
 export default Footer;
