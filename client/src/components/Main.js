e66a49e476474cb5f721b966db420a5f2072d5c0
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-15 14:57:58 +0300

    Add Auth context to main component

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,56 +1,17 @@
-import { Component } from "react";
-import { Link } from "react-router-dom";
-import BookItem from "./hoc/BookItem";
-import internal from "../services/internal";
-import { categories } from "../utils/categories";
+import { useContext } from "react";
+import { Outlet } from "react-router-dom";
 import "../styles/Main.css";
+import Auth from "../contexts/Auth";
+import Aside from "./Aside";
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: [],
-    };
-  }
-
-  componentDidMount() {
-    internal.getAll().then((books) => {
-      this.setState({ books });
-    });
-  }
-
-  render() {
-    return (
-      <div id="site">
-        <div className="categories">
-          <h2>CATEGORIES</h2>
-          <div className="item-categories">
-            <ul>
-              {categories.map((x) => (
-                <li>
-                  <Link to="/">{x}</Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div id="main">
-          {this.state.books?.map((x) => (
-            <BookItem key={x.id} {...x} />
-          ))}
-          <Link
-            to="/"
-            onClick={() => {
-              window.scroll(0, 0);
-            }}
-          >
-            <div className="to-top">
-              <i className="fa-solid fa-arrow-up"></i>
-            </div>
-          </Link>
-        </div>
-      </div>
-    );
-  }
-}
+const Main = () => {
+  let auth = useContext(Auth);
+  return (
+    <div id="site">
+      {auth?.user?.user?.email}
+      <Aside />
+      <Outlet />
+    </div>
+  );
+};
 export default Main;
