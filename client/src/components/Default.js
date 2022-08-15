b6000cd6d25c33876ac607483ddcf71014685099
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-15 14:57:28 +0300

    Move code from Main to Default component due to restructuring

--- client/src/components/Default.js
+++ client/src/components/Default.js
@@ -1,44 +1,70 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import BookItem from "./hoc/BookItem";
 import internal from "../services/internal";
 import "../styles/Main.css";
+import "../utils/ratingCalc";
+import { ratingCalc } from "../utils/ratingCalc";
 
-class Default extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: [],
-    };
-  }
+const Default = () => {
+  const [books, setBooks] = useState([]);
 
-  componentDidMount() {
+  const [searchParams] = useSearchParams();
+  const sort = searchParams.get("sort");
+  let title1 = "";
+  let info1 = (x) => null;
+
+  useEffect(() => {
     internal.getAll().then((books) => {
-      this.setState({ books });
+      setBooks(books);
     });
+  }, []);
+
+  if (sort === "sortCount") {
+    books.sort((item1, item2) => {
+      return (
+        ratingCalc.ratingCount(item2.rating) -
+        ratingCalc.ratingCount(item1.rating)
+      );
+    });
+    title1 = "Most read books";
+    info1 = (x) => ratingCalc.ratingCount(x.rating) + "   votes";
   }
 
-  render() {
-    const arrBooks = this.state.books;
-    return (
-      <div id="site">
-        <div id="main">
-          {arrBooks?.map((x) => (
-            <BookItem key={x.id} {...x} />
-          ))}
-          <Link
-            to="/"
-            onClick={() => {
-              window.scroll(0, 0);
-            }}
-          >
-            <div className="to-top">
-              <i className="fa-solid fa-arrow-up"></i>
-            </div>
-          </Link>
-        </div>
-      </div>
-    );
+  if (sort === "sortAv") {
+    books.sort((item1, item2) => {
+      return (
+        ratingCalc.averageRating(item2.rating) -
+        ratingCalc.averageRating(item1.rating)
+      );
+    });
+    title1 = "Highest rated books";
+    info1 = (x) => "rating : " + ratingCalc.averageRating(x.rating);
   }
-}
+
+  return (
+    <div id="site">
+      <div id="main">
+        <div className="title">{title1}</div>
+        {books?.map((x) => (
+          <div key={x.id} style={{ width: "20%", margin: "1rem" }}>
+            <div className="info">{info1(x)}</div>
+            <BookItem key={x.id} {...x} />
+          </div>
+        ))}
+        <Link
+          to="/"
+          onClick={() => {
+            window.scroll(0, 0);
+          }}
+        >
+          <div className="to-top">
+            <i className="fa-solid fa-arrow-up"></i>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 export default Default;
