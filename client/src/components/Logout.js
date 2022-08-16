12434c1a6d2ae3ad79669d179926e788d56cd18c
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-16 19:17:40 +0300

    Remove erroniously added hook

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useReducer } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Auth from "../contexts/Auth";
 import internal_api from "../services/internal";
