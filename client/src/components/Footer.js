e3d9e42aacef31e67ec4d0905ad9aa5ec9d44e54
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-16 19:10:35 +0300

    Add icons to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,9 +2,14 @@ import "../styles/Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <a href="https://www.goodreads.com/">Goodreads</a>
-      <a href="https://books.google.com/">Google Books</a>
+    <div id="footer">
+      <a href="https://www.goodreads.com/">
+        <i className="fa-solid fa-book-open-reader"></i>Goodreads
+      </a>
+
+      <a href="https://books.google.com/">
+        <i className="fa-solid fa-book-open"></i>Google Books
+      </a>
     </div>
   );
 };
