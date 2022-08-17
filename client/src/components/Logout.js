7a6b6969f534e1428abb76f9bdc235f59fe28a32
Vittorio Rossitto <v.s.r@abv.bg>
2022-08-17 19:59:33 +0300

    Fix logout failure

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,13 +1,13 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Auth from "../contexts/Auth";
-import internal_api from "../services/internal";
+// import internal_api from "../services/internal";
 
 const Logout = () => {
   const navigate = useNavigate();
   const auth = useContext(Auth);
   useEffect(() => {
-    internal_api.logout();
+    // internal_api.logout();
     auth.user = undefined;
     auth.isLoggedIn = false;
     auth.isAdmin = false;
